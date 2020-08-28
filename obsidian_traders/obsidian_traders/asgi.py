684f309dbe2f13e301de556c51ffc26ed804c6f8
Erick Mungai <erick.mbugua20@gmail.com>
2020-08-28 12:43:57 +0300

    Add files via upload

---
+++ obsidian_traders/obsidian_traders/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for obsidian_traders project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'obsidian_traders.settings')
+
+application = get_asgi_application()
